20c3464600f82ad48e9d1ca605ce93a806ccb43b
Brian Woodward <brian.woodward@gmail.com>
2015-09-09 22:14:55 -0400

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * paginationator <https://github.com/doowb/paginationator>
+ *
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
